8470da4e32344484d021dd7e8f9e91aca684c4fc
xelomain <75476391+xelomain@users.noreply.github.com>
2022-05-12 19:12:31 +0200

    Wikipedia link

--- src/views/startpage.tsx
+++ src/views/startpage.tsx
@@ -3,12 +3,10 @@ import Markdown from 'react-markdown';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 
-export default function startpage () {
+const startpage: React.FunctionComponent = () => {
     
-    useEffect(() => {
+    const wikiLink = 'https://de.m.wikipedia.org/wiki/CO2-Bilanz';
 
-    });
-    
     var infotext = '#CO2-Footprint \n Der CO2-Fußabdruck ist ein Maß für den Gesamtbetrag von Kohlenstoffdioxid-Emissionen, der direkt bzw. indirekt durch Aktivitäten oder Lebensstadien von Produkten oder Personen entstehen bzw. verursacht werden. Der CO2-Fußabdruck hat in den letzten Jahren an Bedeutung gewonnen als Mittel, die Klimaauswirkungen von Aktivitäten wie Bereitstellung oder Konsum von Produkten und Dienstleistungen für einzelne Personen oder aggregiert für Organisationen und Staaten zu ermitteln. Auf dieser Basis können gezieltere Klimaschutz-Maßnahmen ergriffen werden, um angestrebte Klimaziele zu erreichen, z. B. die globale Erwärmung auf maximal 2 °C Temperaturerhöhung zu begrenzen.';
     
     return (
@@ -23,7 +21,7 @@ export default function startpage () {
                 <Button variant="contained" size="large">
                     Welt CO2-Fußabdruck
                 </Button>
-                <Button variant="contained" size="large">
+                <Button variant="contained" size="large" href={wikiLink}>
                     Wikipedia
                 </Button>
             </div>
@@ -31,3 +29,5 @@ export default function startpage () {
     </Box>
     );
 }
+
+export default startpage;
