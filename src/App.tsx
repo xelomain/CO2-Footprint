07927e4b29e1a742aff46fef66958fe0010b2a3d
xelomain <75476391+xelomain@users.noreply.github.com>
2022-05-13 14:48:47 +0200

    App grundgeruest

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import './App.css';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import startpage  from './views/startpage';
 
-function App() {
+
+export const App = () => {
   return (
     <div className="App">
       <header className="CO2-Footprint">
       </header>
+      <Router>
+        <Switch>
+          <Route path="*">
+            <Switch>
+              <Route path="/startpage">
+                <startpage />
+              </Route>
+              <Route path="/rechner">
+                
+              </Route>
+              <Route path="/welt-co2-fußabdruck">
+
+              </Route>
+              <Route path="/wikipedia">
+
+              </Route>
+            </Switch>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
-
-export default App;
