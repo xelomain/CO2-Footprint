cd00f6a6803b37b780832a9d52aff82a3b041bc0
xelomain <75476391+xelomain@users.noreply.github.com>
2022-06-22 18:51:32 +0200

    verpiss dich selbst react möchte dich nicht

--- src/views/rechner.tsx
+++ src/views/rechner.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import Radio from '@mui/material/Radio';
 import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
